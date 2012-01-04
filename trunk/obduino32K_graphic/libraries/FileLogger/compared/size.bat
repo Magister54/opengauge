17fe086baa640e9c58bb64b9ba69bc6937240e07
eimixas@gmail.com <eimixas@gmail.com@845ddca0-e750-0410-b601-b11625e3154d>
2012-01-04 10:24:50 +0000

    Removed temporary files.

--- trunk/obduino32K_graphic/libraries/FileLogger/compared/size.bat
+++
@@ -1,5 +0,0 @@
-echo Object sizes > binary-sizes.txt
-..\..\tools\avr\bin\avr-size FileLogger.o >> binary-sizes.txt
-..\..\tools\avr\bin\avr-size nanofat.o >> binary-sizes.txt
-..\..\tools\avr\bin\avr-size mmc.o >> binary-sizes.txt
-..\..\tools\avr\bin\avr-size Spi.o >> binary-sizes.txt
