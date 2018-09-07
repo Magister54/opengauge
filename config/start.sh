bf06a090909a0e8530a301d9817c6e217df7bef7
Adrien Lessard <adrien.lessard.42@gmail.com>
2018-09-07 17:14:15 -0400

    doc

---
+++ config/start.sh
@@ -0,0 +1,7 @@
+
+# Make sure the serial port is readable / writable
+sudo chmod 660 /dev/ttyS0
+
+# Run the executable (you may need to change the location)
+/home/pi/Documents/git/dashboard/pi/bin/dashboard &
+
